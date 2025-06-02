1a7d420687424cc983a7091225f34760923dc424
CarlosMattei <carlosmattei2007@gmail.com>
2025-06-02 12:27:03 -0300

    rotas iniciais

--- App.js
+++ App.js
@@ -1,27 +1,42 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, TextInput, Image, ScrollView } from "react-native";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from "react-native-vector-icons/Ionicons";
 
+import Home from './pages/home';
+import Perfil from './pages/perfil';
+import Cadastro from './pages/cadastro';
+import Login from './pages/login';
+import NovaMeta from './pages/nova-meta';
 
+const Tab = createBottomTabNavigator();
 
-class App extends Component {
-
-  render() {
-    return (
-      <View style={styles.container}>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home" screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#0071fe"
+      }}>
+        <Tab.Screen name="Home" component={Home} options={{tabBarIcon: ({color, size}) => (
+          <Ionicons name="home-outline"
+           color={color}
+           size={size}
+           />
+        )}} />
+        <Tab.Screen name="Perfil" component={Perfil} options={{tabBarIcon: ({color, size}) => (
+          <Ionicons name="person-outline"
+           color={color}
+           size={size}
+           />
+        )}} />
+        <Tab.Screen name="NovaMeta" component={NovaMeta} options={{tabBarIcon: ({color, size}) => (
+          <Ionicons name="add-outline"
+           color={color}
+           size={size}
+           />
+        )}} />
+      </Tab.Navigator>
+    </NavigationContainer>  
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    width: 300,
-    height : 300,
-  }
-  
-});
-
-export default App;
---
+++ pages/nova-meta.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function NovaMeta() {
+    return (
+        <View>
+            <Text>Nova Meta</Text>
+        </View>
+    );
+}
